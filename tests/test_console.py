ed0cf5409c9a6adfce93d4d6d88dc1652eb9daf9
Iano-theDev <sinceianmike@gmail.com>
2022-10-30 18:02:16 +0300

    fix pycodestyle

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest for the console
+"""
+
+
+import unittest
+import console
+import pep8
+import inspect
+HBNBCommand = console.HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """Tests for documentation of the console"""
+    pass
